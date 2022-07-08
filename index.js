9d7dd908b42621421ad608bc0668c94a0cc50ab7
marcodelpontmatias <marcodelpontmatias@gmail.com>
2022-07-08 18:34:25 -0300

    se quitaron los test de funcionamiento

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-alert("test");
-let personas = [
-  {nombre: "Matias"},
-  {nombre: "Julian"}
-];
-let body = document.getElementsByTagName("body")[0];
-personas.forEach(function(persona){
-  let h2 = document.createElement("h2");
-  h2.innerHTML = persona.nombre;
-  body.appendChild(h2);
-});
+alert("Pulse aceptar para continuar");
